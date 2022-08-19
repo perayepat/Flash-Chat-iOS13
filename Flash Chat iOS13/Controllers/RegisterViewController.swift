05344b2e58c448c35912a3bb353251c47b73bd89
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-08-19 17:53:26 +0200

    :notebook: update

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,17 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        let email = emailTextfield.text ?? ""
+        let password = passwordTextfield.text ?? ""
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                //localized Description will have the error pop up in the users language
+                print(e.localizedDescription)
+            } else{
+                //Go to the chat view controller, wtih segue actions
+                self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                
+            }
+        }
     }
-    
 }
